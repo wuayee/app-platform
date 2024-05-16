4ee36be49e7dc56585c724255f5e291130b859c2
* <*>
2024-05-16 20:29:59 +0800

    Match-id-7e3f9cd46179e3cfb6f7c826c338540164a20608

---
+++ framework/data-bus/core/test/common/utils/FileUtilsTest.cpp
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+ * Description: test for common/utils/FileUtils
+ */
+
+#include <gtest/gtest.h>
+
+#include "utils/FileUtils.h"
+
+using namespace DataBus::Common;
+
+namespace DataBus {
+namespace Test {
+
+class FileUtilsTest : public testing::Test {
+};
+
+TEST_F(FileUtilsTest, CreateDirectory)
+{
+    std::string testDir = "test_dir/sub_dir";
+    FileUtils::CreateDirectory(testDir);
+    struct stat info{};
+    EXPECT_EQ(0, stat("test_dir", &info));
+    EXPECT_TRUE(S_ISDIR(info.st_mode));
+    EXPECT_EQ(0, stat("test_dir/sub_dir", &info));
+    EXPECT_TRUE(S_ISDIR(info.st_mode));
+    FileUtils::RemoveDirectory("test_dir");
+}
+
+TEST_F(FileUtilsTest, RemoveDirectory)
+{
+    std::string testDir = "test_dir";
+    FileUtils::CreateDirectory(testDir);
+    // 创建一个文件以测试递归删除
+    std::ofstream(testDir + "/test_file.txt").close();
+    FileUtils::CreateDirectory(testDir + "/sub_dir");
+    std::ofstream(testDir + "/sub_dir/test_file2.txt").close();
+    EXPECT_TRUE(FileUtils::RemoveDirectory(testDir));
+    struct stat info{};
+    EXPECT_NE(0, stat(testDir.c_str(), &info));
+}
+
+TEST_F(FileUtilsTest, CreateFileIfNotExists)
+{
+    std::string testFilePath = "test_dir/test_file.txt";
+    FileUtils::CreateFileIfNotExists(testFilePath);
+    std::ifstream fileCheck(testFilePath);
+    EXPECT_TRUE(fileCheck.good());
+    fileCheck.close();
+    FileUtils::RemoveDirectory("test_dir");
+}
+} // namespace Test
+} // namespace DataBus
