0285cc9117d39b614f794244f6ab58fe80e8ddf4
* <*>
2024-05-20 10:41:47 +0800

    Match-id-bc4160fc70f6a02aea53d25bbd8a4c06ebd58780

---
+++ app-engine/frontend/src/components/HeaderMenus/index.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Breadcrumb } from 'antd';
+import Link from 'antd/es/typography/Link';
+
+function itemRender(currentRoute, params, items, paths) {
+  const navigate=useNavigate();
+  const isLast = currentRoute?.path === items[items.length - 1]?.path;
+  return isLast ? (
+    <span>{currentRoute.title}</span>
+  ) : (
+    <Link onClick={()=>{
+    navigate(currentRoute.path);}}>{currentRoute.title}</Link>
+  );
+}
+
+const HeaderMenus: React.FC<{items}> = ({items}) => {
+  return(
+      <Breadcrumb itemRender={itemRender} items={items} />
+)};
+
+export default HeaderMenus;
