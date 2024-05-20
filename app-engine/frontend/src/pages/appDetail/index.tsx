9a2abbf0cb263a7309b9dfd0d35a8880b0184050
* <*>
2024-05-20 10:04:38 +0800

    Match-id-65837085d0e2d594b83a39a94e04dccf38517e39

--- app-engine/frontend/src/pages/appDetail/index.tsx
+++ app-engine/frontend/src/pages/appDetail/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoBack from '../../components/go-back/GoBack';
+import { Tabs } from 'antd';
+import AppAnalyse from './analyse';
 
-const AppDetail: React.FC = () => (
+const onChange = (key: string) => {
+  console.log(key);
+};
+
+const AppDetail: React.FC = () => {
+  const items: TabsProps['items'] = [
+    {
+      key: '1',
+      label: '概览',
+      children: 'Content of Tab Pane 1'
+    },
+    {
+      key: '2',
+      label: '分析',
+      children: <AppAnalyse></AppAnalyse>,
+    },
+    {
+      key: '3',
+      label: '反馈',
+      children: 'Content of Tab Pane 3',
+    },
+    {
+      key: '4',
+      label: '评估',
+      children: 'Content of Tab Pane 4',
+    },
+  ];
+  return(
   <div className='aui-fullpage'>
     <div className='aui-header-1'>
       <div className='aui-title-1'><GoBack path={'/app'} title='应用详情'/></div>
     </div>
-    <div className='aui-block'>
-      <div className='dc-table-operate'>
-        <div>ops-left</div>
-        <div>ops-right</div>
-      </div>
+    <div className='aui-block' style={{paddingTop:0}}>
+       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
       <div />
     </div>
   </div>
-);
+)};
 
 export default AppDetail;
