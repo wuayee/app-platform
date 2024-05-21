24efa6f5e8a85848abac63fcd15f31841eb0370b
* <*>
2024-05-21 12:27:30 +0800

    Match-id-fbec88ea2dcbcd65409c1d4ea5880b43fe51e9d6

--- app-engine/frontend/src/pages/apps/components/appCard/index.tsx
+++ app-engine/frontend/src/pages/apps/components/appCard/index.tsx
@@ -6,17 +6,25 @@ import { url } from "inspector";
 import { Icons } from "../../../../components/icons";
 import "./style.scoped.scss";
 
-
-export interface knowledgeBase {
-    name: string;
-    createBy: string;
-    // icon: () => ReactElement;
-    desc: string;
-    id: string;
+function Avatar(){
+  const employeeNumber ="60032692"
+  return (
+    <div
+      style={{
+        width: "18px",
+        height: "18px",
+        borderRadius: "50%",
+        overflow: "hidden",
+        background:
+          `url(https://w3.huawei.com/w3lab/rest/yellowpage/face/${employeeNumber}/120)`,
+        backgroundSize:'contain'
+      }}
+    >
+    </div>
+  );
 }
 
 const AppCard = ({ cardInfo }: any) => {
-    console.log("cardInfo", cardInfo.attributes.icon);
     const operatorItems: MenuProps["items"] = [
         {
             key: "delete",
@@ -27,79 +35,48 @@ const AppCard = ({ cardInfo }: any) => {
         console.log("info", info);
     };
     return (
-      //   <div className="app_card_root">
       <div
         className="app_card_root"
         style={{
-          background:"url(/src/assets/images/knowledge/knowledge-background.png)",
+          background:
+            "url(/src/assets/images/knowledge/knowledge-background.png)",
         }}
       >
-        <div className="content">
-          {/* 头部区域 */}
-          <div className="app_card_header">
-            <img src="/src/assets/images/knowledge/knowledge-base.png" />
-            <div className="infoArea">
-              <div className="headerTitle">{cardInfo.name}</div>
-              <div className="title_info" style={{ display: "flex", alignItems: "center" }}>
-                <img
-                  width={18}
-                  height={18}
-                  style={{ marginRight: "10px" }}
-                  src="/src/assets/images/knowledge/knowledge-base.png"
-                />
-                <div className="createBy">{cardInfo.createBy}</div>
-              </div>
-            </div>
-          </div>
-
-          {/* 描述 */}
-          <div
-            className="body"
-            style={{
-              wordBreak: "break-all",
-              marginTop: 16,
-              fontSize: "14px",
-              lineHeight: "22px",
-              textAlign: "justify",
-              flex: 1,
-            }}
-          >
-            {cardInfo.attributes?.description}
-          </div>
-
-          {/* 底部 */}
-          <div
-            className="footer"
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              marginTop: 16,
-            }}
-          >
-            <div className="operator">
-              <Dropdown
-                menu={{
-                  items: operatorItems,
-                  onClick: (info) => {
-                    clickItem(info);
-                  },
-                }}
-                placement="bottomLeft"
-                trigger={["click"]}
-              >
-                <div
-                  style={{
-                    cursor: "pointer",
-                  }}
-                >
-                  <Icons.more width={20} />
-                </div>
-              </Dropdown>
+        {/* 头部区域 */}
+        <div className="app_card_header">
+          <img src="/src/assets/images/knowledge/knowledge-base.png" />
+          <div className="infoArea">
+            <div className="headerTitle">{cardInfo.name}</div>
+            <div className="title_info" style={{display:'flex',alignItems:'center'}}>
+              <Avatar/>
+              <div className="createBy">{cardInfo.createBy}</div>
             </div>
           </div>
         </div>
+
+        {/* 描述 */}
+        <div className="app_card_body">{cardInfo.attributes?.description}</div>
+
+        {/* 底部 */}
+        <div className="app_card_footer">
+          <div className="operator">
+            <Dropdown
+              menu={{
+                items: operatorItems,
+                onClick: (info) => {
+                  clickItem(info);
+                },
+              }}
+              placement="bottomLeft"
+              trigger={["click"]}
+            >
+              <div style={{ cursor: "pointer" }}>
+                <Icons.more width={20} />
+              </div>
+            </Dropdown>
+          </div>
+        </div>
       </div>
-      //   </div>
     );
 };
 
--- app-engine/frontend/src/pages/apps/index.tsx
+++ app-engine/frontend/src/pages/apps/index.tsx
@@ -1,77 +1,71 @@
 import React, { useState, useEffect } from "react";
-import { Button, Input } from "antd";
+import { Button, Divider, Input } from "antd";
 import { Icons } from "../../components/icons";
 import { queryAppsApi } from "../../shared/http/apps.js";
 import AppCard from "./components/appCard";
 import './index.scoped.scss'
+import { debounce } from "../../shared/utils/common";
+import { HashRouter, Route, useNavigate, Routes } from "react-router-dom";
 
 const Apps: React.FC = () => {
-    const create = () => {
-        console.log("创建");
-    };
-
+    // 数据初始化
     const [appData, setAppData] = useState([]);
+    const [oriData, setOriData] = useState([]);
     const queryApps = async () => {
         const res: any = await queryAppsApi();
         setAppData(res.data);
-        console.log("res", res.data);
+        setOriData(res.data);
+        console.log("res", res.data,oriData);
     };
     useEffect(() => {
-        queryApps();
+      queryApps();
     }, []);
 
+    // 创建
+    const navigate = useNavigate();
+    const create = () => {
+      navigate("/knowledge-base/create");
+    };
+
+    // 搜索
+    function onSearchValueChange(value: string) {
+      const arr = oriData.filter((v: any) => v.name.includes(value));
+      setAppData(arr);
+    }
+    const handleSearch = debounce(onSearchValueChange, 500);
+    function clickCard(item:any){
+        console.log('item',item)
+    }
+
     return (
-        <div className="aui-fullpage">
-            <div className="aui-header-1">
-                <div className="aui-title-1">应用市场</div>
-            </div>
-            <div className="aui-block">
-                <div
-                    className="operatorArea"
-                    style={{
-                        display: "flex",
-                        gap: "16px",
-                    }}
-                >
-                    <Button
-                        type="primary"
-                        style={{
-                            background: "#2673E5",
-                            width: "96px",
-                            height: "32px",
-                            fontSize: "14px",
-                            borderRadius: "4px",
-                            letterSpacing: "0",
-                        }}
-                        onClick={create}
-                    >
-                        创建
-                    </Button>
-                    <Input
-                        placeholder="搜索"
-                        style={{
-                            width: "200px",
-                            borderRadius: "4px",
-                            border: "1px solid rgb(230, 230, 230)",
-                        }}
-                        prefix={<Icons.search color={"rgb(230, 230, 230)"} />}
-                    />
-                </div>
-                <div className="card_list">
-                    {appData.map((item: any) => (
-                        <>
-                            <AppCard
-                                key={item.id}
-                                cardInfo={item}
-                                style={{
-                                    flex: "0",
-                                }}
-                            />
-                        </>
-                    ))}
-                </div>
-            </div>
+      <div className="aui-fullpage">
+        <div className="aui-header-1">
+          <div className="aui-title-1">应用市场</div>
         </div>
+        <div className="aui-block">
+          <div className="operatorArea">
+            <Button type="primary" onClick={create}>
+              创建
+            </Button>
+            <Input
+              placeholder="搜索"
+              style={{ width: "200px", height: "35px", marginLeft: "16px" }}
+              prefix={<Icons.search color={"rgb(230, 230, 230)"} />}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+          </div>
+    
+          {appData.length > 0 ? (
+            <div className="card_list">
+              {appData.map((item: any) => (
+                <div onClick={() => clickCard(item)}>
+                  <AppCard key={item.id} cardInfo={item} />
+                </div>
+              ))}
+            </div>
+          ): <div>暂无数据</div>}
+        </div>
+      </div>
     );
 };
 export default Apps;
