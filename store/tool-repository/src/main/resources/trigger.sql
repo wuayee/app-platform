7df2ed54508f4b5de5e25ab0f6f20aa994cd4b00
* <*>
2024-05-21 20:14:05 +0800

    Match-id-ac5df6f825e743ee21d841a112f1b929d75e738d

--- store/tool-repository/src/main/resources/trigger.sql
+++ store/tool-repository/src/main/resources/trigger.sql
@@ -8,13 +8,13 @@ end;
 $$ language plpgsql;
 
 -- 为 store_tool 表创建触发器
-create trigger trigger_update_tool
+create or replace trigger trigger_update_tool
     before update ON store_tool
     for each row
 execute function update_when_update();
 
 -- 为 store_tag 表创建触发器
-create trigger trigger_update_tag
+create or replace trigger trigger_update_tag
     before update ON store_tag
     for each row
 execute function update_when_update();
