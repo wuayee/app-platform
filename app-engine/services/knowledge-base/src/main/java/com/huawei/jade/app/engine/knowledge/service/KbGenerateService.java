7bc8a69276e84fb35c2842803ed3c09b1862d98d
* <*>
2024-05-21 15:00:37 +0800

    Match-id-fb87f892a7a30cc01cb1caf5e57627fa2b0f217b

---
+++ app-engine/services/knowledge-base/src/main/java/com/huawei/jade/app/engine/knowledge/service/KbGenerateService.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ */
+
+package com.huawei.jade.app.engine.knowledge.service;
+
+import com.huawei.fitframework.annotation.Genericable;
+import com.huawei.jade.app.engine.knowledge.dto.KbGenerateConfigDto;
+
+/**
+ * KbGenerateService 知识生成
+ *
+ * @author YangPeng
+ * @since 2024-05-20 11:10
+ */
+public interface KbGenerateService {
+    /**
+     * 导入知识
+     *
+     * @param configDto 配置信息
+     */
+    @Genericable(id = "com.huawei.jade.app.engine.knowledge.service.KbGenerateService.importKnowledge")
+    void importKnowledge(KbGenerateConfigDto configDto);
+}
