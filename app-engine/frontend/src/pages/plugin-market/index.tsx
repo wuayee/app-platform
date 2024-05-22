94743d81129d32bd4ca88908c086d4e8b5895120
* <*>
2024-05-22 16:00:11 +0800

    Match-id-e546d7d109d06ff4c8b196a979da2444d21d867b

---
+++ app-engine/frontend/src/pages/plugin-market/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Avatar, Space, Tabs, Tag } from 'antd';
+import data from './data';
+import './style.scoped.scss';
+import { AppDefaultIcon } from '../../assets/icon';
+import { StarFilled, UserOutlined } from '@ant-design/icons';
+
+const PluginMarket = () => {
+  return (
+    <div className='aui-fullpage'>
+      <div className='aui-block plugin-market'>
+        <Tabs
+          defaultActiveKey='1'
+          items={[
+            {
+              label: '市场',
+              key: '1',
+            },
+            {
+              label: '我的',
+              key: '2',
+            },
+          ]}
+        />
+        <div className='plugin-wrapper'>
+          {data.map((item) => (
+            <div className='plugin-item'>
+              <div className='plugin-item-header'>
+                <div className='plugin-item-header-icon'>
+                  <AppDefaultIcon />
+                </div>
+                <div className='plugin-item-header-title'>
+                  <div className='plugin-item-title-1'>{item.name}</div>
+                  <div className='plugin-item-title-2'>
+                    <Avatar size={16} src='https://api.dicebear.com/7.x/miniavs/svg?seed=1' />
+                    <Space>
+                      张小明
+                      <div className='plugin-item-tags'>
+                        {item.tags.map((item) => (
+                          <Tag>{item}</Tag>
+                        ))}
+                      </div>
+                    </Space>
+                  </div>
+                </div>
+              </div>
+              <div className='plugin-item-content'>{item.description}</div>
+              <div className='plugin-item-footer'>
+                <div className='plugin-item-footer-left'>
+                  <Space className='plugin-item-footer-icons'>
+                    <UserOutlined /> 2.3k
+                  </Space>
+                  <Space className='plugin-item-footer-icons'>
+                    <StarFilled /> 2.3k
+                  </Space>
+                </div>
+                <div className='plugin-item-footer-right'></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PluginMarket;
