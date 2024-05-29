9e88f91174b72eb490ca73b52bd5f71aa3198488
* <*>
2024-05-29 19:54:06 +0800

    Match-id-c43c5e53a68959718efae18bcb07384f662337dc

--- app-engine/frontend/src/pages/appDetail/analyse/index.tsx
+++ app-engine/frontend/src/pages/appDetail/analyse/index.tsx
@@ -3,12 +3,14 @@ import * as echarts from 'echarts';
 import './style.scoped.scss';
 import { float } from 'html2canvas/dist/types/css/property-descriptors/float';
 import { Card, Select } from 'antd';
+import { getAnalysisData } from '../../../shared/http/apps';
+import { useParams } from 'react-router-dom';
 
 const timeOption = [
-  { value: '1', label: '过去1天' },
-  { value: '2', label: '过去7天 ' },
-  { value: '3', label: '过去1月' },
-  { value: '4', label: '过去半年' },
+  { value: 'YESTERDAY', label: '过去1天' },
+  { value: 'LAST_7_DAYS', label: '过去7天 ' },
+  { value: 'LAST_MONTH', label: '过去1月' },
+  // { value: '4', label: '过去半年' },
 ];
 
 const top5UserOption = {
@@ -21,11 +23,9 @@ const top5UserOption = {
   },
   yAxis: {
     type: 'value',
-    max: 100, // 取100为最大刻度
     axisLabel: {
       show: true,
       interval: 'auto',
-      formatter: '{value} %',
     },
   },
   series: [
@@ -47,7 +47,6 @@ const top5DepartmentOption = {
   },
   yAxis: {
     type: 'value',
-    max: 100, // 取100为最大刻度
     axisLabel: {
       show: true,
       interval: 'auto',
@@ -123,17 +122,20 @@ const AnalyseCard = ({ info }) => (
       <div>
         <span className='number'>{info?.num}</span>
         <span className='number-unit'>{info?.unit}</span>
-        <div className='number-tip'>
+        {/* <div className='number-tip'>
           <span>{info?.sign==='down'? '相比昨日下降':'相比昨日上升'}</span>
           <span style={{ color: info?.sign==='down'?'#c63939':'#53ab6b' }}> {info?.percent}%</span>
-        </div>
+        </div> */}
       </div>
       <img width={60} height={65} src='/src/assets/images/knowledge/knowledge-base.png' />
     </div>
   </Card>
 );
-const ChartCard = ({ info }) => (
-  <Card className='chart-card' style={{ height: info.height + 50 }}>
+const ChartCard = ({ info }: any) => (
+  <Card className='chart-card' style={{ 
+    height: info.height + 50, 
+    width: `${info?.width ?? '49.5%'}` 
+  }}>
     <div className='title'>{info.title}</div>
     <div id={info.id} className='chart' style={{ height: info.height }} />
   </Card>
@@ -141,58 +143,93 @@ const ChartCard = ({ info }) => (
 
 const AppAnalyse: React.FC = () => {
   const [graphData,setgraphData]=useState({allRequest:{},allActive:{},avgSpeed:{}});
-  const initData = () => {
-    const allRequest={title:'总请求数',num:2523,unit:'个',sign:'down',percent:2};
-    const allActive={title:'总活跃用户数',num:1324,unit:'个',sign:'up',percent:2};
-    const avgSpeed={title:'平均相应速度',num:100,unit:'ms',sign:'down',percent:2};
-    setgraphData({allRequest,allActive,avgSpeed})
+  const [time, setTime]=useState('LAST_MONTH');
+  const [total, setTotal]=useState(0);
 
-    top5UserOption.xAxis.data = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    top5UserOption.series[0].data= [20, 56, 15, 80, 70, 10, 30];
-
-    top5DepartmentOption.xAxis.data = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    top5DepartmentOption.series[0].data = [25, 37, 75, 90, 450, 10, 30];
-
-    speedOption.series[0].data = [
-      { value: 48, name: '500ms以下' },
-      { value: 32, name: '500-1000ms' },
-      { value: 20, name: '10001-2000ms' },
-    ];
-
-    tradeOption.xAxis.data = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    tradeOption.series[0].data = [820, 932, 901, 934, 1290, 1330, 1320];
-  };
-
-  const handleSelectTimeChange = (value: string) => {
-    initData();
-    console.log(`selected ${value}`);
-  };
+  const { tenantId, appId} = useParams();
 
   useEffect(() => {
-    initData();
     const tradeChart = echarts.init(document.getElementById('trade'));
     const speedChart = echarts.init(document.getElementById('speed'));
     const top5UserChart = echarts.init(document.getElementById('top5User'));
-    const top5DepartmentChart = echarts.init(document.getElementById('top5Department'));
-    tradeChart.setOption(tradeOption);
-    speedChart.setOption(speedOption);
-    top5UserChart.setOption(top5UserOption);
-    top5DepartmentChart.setOption(top5DepartmentOption);
+    getAnalysisDataByTime(top5UserChart, speedChart, tradeChart);
     window.addEventListener('resize', function () {
       tradeChart.resize();
       speedChart.resize();
       top5UserChart.resize();
-      top5DepartmentChart.resize();
     });
-  }, []);
+  }, [time]);
+
+
+  // 设置Tab数据
+  const setTabData = (metrics: any) => {
+    const allRequest={title:'总请求数',num:metrics?.total_requests?.value ?? 0 ,unit:'个',sign:'down',percent:2};
+    const allActive={title:'总活跃用户数',num:metrics?.total_active_users?.value ?? 0,unit:'个',sign:'up',percent:2};
+    const avgSpeed={title:'平均响应速度',num:metrics?.average_response_time?.value ?? 0,unit:'ms',sign:'down',percent:2};
+    setgraphData({allRequest,allActive,avgSpeed})
+  }
+
+  // 设置Top5用户数据
+  const setTop5UserData = (topUsers: any, top5UserChart) => {
+
+    const x = (topUsers?? []).map(user=> (user?.createUser || ''));
+    const y = (topUsers?? []).map(user=> (user?.accessCount || 0));
+    top5UserOption.xAxis.data = x;
+    top5UserOption.series[0].data= y;
+    top5UserChart.setOption(top5UserOption);
+  }
+
+  // 设置平均响应速度
+  const setAvargeSpeed = (avgResponseRange, speedChart) => {
+    let totalData =0;
+
+    const data = Object.keys(avgResponseRange || {}).map(item=> {
+      totalData +=  avgResponseRange[item]?.count || 0;
+      return ({
+        value: avgResponseRange[item]?.count || 0,
+        name: avgResponseRange[item]?.range || ''
+      })
+    });
+    setTotal(totalData)
+    speedOption.series[0].data = data;
+    speedChart.setOption(speedOption);
+  }
+
+  // 设置用户访问趋势
+  const setUserTrade = (userAccessData, speedChart) => {
+    const xData = (userAccessData || []).map(item=> (item.time_unit))
+    const xValue = (userAccessData || []).map(item=> (item.access_count))
+    tradeOption.xAxis.data = xData;
+    tradeOption.series[0].data = xValue;
+    speedChart.setOption(tradeOption);
+  }
+
+  // 获取分析数据
+  const getAnalysisDataByTime = async (top5UserChart, speedChart, tradeChart) => {
+    try {
+      const res = await getAnalysisData({
+        appId: appId,
+        timeType: time,
+      });
+      setTabData(res?.basicMetrics);
+      setTop5UserData(res?.topUsers, top5UserChart);
+      setAvargeSpeed(res?.avgResponseRange, speedChart);
+      setUserTrade(res?.userAccessData, tradeChart)
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div>
       <div className='select'>
         <Select
           className='select-time'
-          defaultValue='2'
+          defaultValue={
+            time
+          }
           style={{ width: 120 }}
-          onChange={handleSelectTimeChange}
+          onChange={setTime}
           options={timeOption}
         />
       </div>
@@ -202,21 +239,23 @@ const AppAnalyse: React.FC = () => {
         <AnalyseCard info={graphData.avgSpeed}/>
       </div>
       <div className='chart-cards'>
-        <ChartCard info={{ id: 'trade', height: 350,title:'用户访趋势' }} />
+        <ChartCard info={{ id: 'top5User', title: 'Top5用户', height: 300 }} />
         <Card className='chart-card'>
-          <div className='title'>平均相应速度</div>
+          <div className='title'>平均响应速度</div>
           <div style={{ display: 'flex' }}>
             <div id='speed' style={{ width: 600, height: 250 }} />
             <div className='speed-number'>
-              <div className='number'>100</div>
+              <div className='number'>{total}</div>
               <div>数据条款</div>
             </div>
           </div>
         </Card>
       </div>
-      <div className='chart-cards'>
-        <ChartCard info={{ id: 'top5User', title: 'Top5用户', height: 300 }} />
-        <ChartCard info={{ id: 'top5Department', title: 'Top5部门', height: 300 }} />
+      <div className='chart-cards' style={{
+        width: '100%'
+      }}>
+        <ChartCard info={{ id: 'trade', height: 350,title:'用户访趋势', width:'100%'  }} />
+        {/* <ChartCard info={{ id: 'top5Department', title: 'Top5部门', height: 300 }} /> */}
       </div>
     </div>
   );
