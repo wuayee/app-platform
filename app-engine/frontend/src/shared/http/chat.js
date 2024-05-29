7cb644cf00de05c8c4bf4d494f519547f29b87a3
* <*>
2024-05-29 21:06:08 +0800

    Match-id-247a174e68a2cb97eea96e7e5da6e5f0ee5cb0ad

--- app-engine/frontend/src/shared/http/chat.js
+++ app-engine/frontend/src/shared/http/chat.js
@@ -1,7 +1,7 @@
 import {del, get, post, patch} from "./http";
 import { httpUrlMap } from './httpConfig';
 
-const { AI_URL } = httpUrlMap[process.env.NODE_ENV];
+const { AI_URL, PLUGIN_URL } = httpUrlMap[process.env.NODE_ENV];
 
 // 聊天界面---详情
 export function queryAppDetail(id) {
@@ -11,24 +11,24 @@ export function queryAppDetail(id) {
 
 // 点赞点灭
 export function feedbacksRq(params) {
-  const url = `${AI_URL}/aipp/usr/feedback`;
+  const url = `${PLUGIN_URL}/aipp/usr/feedback`;
   return post(url, params);
 }
 
 // 获取点赞点灭详情
 export function queryFeedback(id) {
-  const url = `${AI_URL}/aipp/usr/feedback/${id}`;
+  const url = `${PLUGIN_URL}/aipp/usr/feedback/${id}`;
   return get(url);
 }
 
 // 更新点赞点灭
 export function updateFeedback(id, data) {
-  const url = `${AI_URL}/aipp/usr/feedback/${id}`;
+  const url = `${PLUGIN_URL}/aipp/usr/feedback/${id}`;
   return patch(url, data);
 }
 
 // 取消点赞点灭
 export function deleteFeedback(id) {
-  const url = `${AI_URL}/aipp/usr/feedback/${id}`;
+  const url = `${PLUGIN_URL}/aipp/usr/feedback/${id}`;
   return del(url);
 }
