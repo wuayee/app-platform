a69cb12d6bf871b2cf82364ccc1ef4e0fee3d64e
* <*>
2024-05-30 16:22:57 +0800

    Match-id-5f77a78a03b9ec84e18c732c2d722e733a1ef1fe

--- framework/elsa/fit-elsa-react/src/components/llm/SkillForm.jsx
+++ framework/elsa/fit-elsa-react/src/components/llm/SkillForm.jsx
@@ -4,6 +4,7 @@ import {useDataContext, useDispatch, useShapeContext} from "@/components/Default
 import "../common/style.css";
 import {JadeStopPropagationSelect} from "../common/JadeStopPropagationSelect.jsx";
 import PropTypes from "prop-types";
+import HttpUtil from "@/components/util/httpUtil.jsx";
 
 const {Panel} = Collapse;
 
@@ -22,104 +23,116 @@ SkillForm.propTypes = {
  * @returns {JSX.Element} 大模型节点技能表单的DOM。
  */
 export default function SkillForm({toolOptions, workflowOptions, config}) {
-    const shape = useShapeContext();
     const data = useDataContext();
     const dispatch = useDispatch();
     const tool = data.inputParams.find(item => item.name === "tools");
     const workflow = data.inputParams.find(item => item.name === "workflows");
+    const aippUrl = config.urls.aippUrl;
 
-    const handleClick = (event) => {
+    // 新增工具流
+    const handleAddWaterFlow = async (event) => {
+        event.stopPropagation(); // 阻止事件冒泡
         if (!config || !config.params || !config.params.tenantId || !config.params.appId) {
             console.error('Cannot get config.params.tenantId or config.params.appId.');
         } else {
-            // 使用 window.open() 打开一个新页面
-            window.open('/appbuilder/#/aipp/' + config.params.tenantId + '/addFlow/' + config.params.appId, '_blank');
-            event.stopPropagation(); // 阻止事件冒泡
+            const timeStr = new Date().getTime().toString();
+            HttpUtil.post(`${aippUrl}/${(config.params.tenantId)}/app/${'df87073b9bc85a48a9b01eccc9afccc3'}`, {
+                type: 'waterFlow',
+                name: timeStr
+            }, null, (response) => {
+                if (response.code === 0) {
+                    window.open(`#/app-develop/${config.params.tenantId}/app-detail/add-flow/${(response.data.id)}`);
+                }
+            }, (error) => {
+                console.log("create aipp error errorMsg=", error)
+            });
         }
-    };
+    }
 
     // Filter `option.label` match the user type `input`
     const filterOption = (input, option) =>
-        (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
 
     const handleSkillChange = (itemId, value) => {
         dispatch({actionType: "changeSkillConfig", id: itemId, value: value});
     };
 
     return (
-        <Collapse bordered={false} className="jade-collapse-custom-background-color" defaultActiveKey={["skillPanel"]}>
-            {
-                <Panel
-                    key={"skillPanel"}
-                    header={
-                        <div className="panel-header"
-                             style={{display: 'flex', alignItems: 'center', justifyContent: "flex-start"}}>
-                            <span className="jade-panel-header-font">技能</span>
-                        </div>
-                    }
-                    className="jade-panel"
-                >
-                    <Row gutter={16} style={{marginBottom: "6px", marginRight: 0, marginLeft: "-3%"}}>
-                        <Col span={21}>
-                            <span className="jade-font-size jade-font-color" style={{marginLeft: "6px"}}>工具</span>
-                        </Col>
-                        {/*430演示工具不需要+号跳转，暂时屏蔽*/}
-                        {/*<Col span={3}>*/}
-                        {/*    <Button type="text" className="icon-button"*/}
-                        {/*            style={{height: "22px", marginLeft: "4px"}}*/}
-                        {/*            onClick={(event) => {*/}
-                        {/*                handleClick(event);*/}
-                        {/*            }}>*/}
-                        {/*        <PlusOutlined/>*/}
-                        {/*    </Button>*/}
-                        {/*</Col>*/}
-                    </Row>
-                    <Form.Item>
-                        <JadeStopPropagationSelect
-                            mode="multiple"
-                            showSearch
-                            allowClear
-                            className="jade-select"
-                            placeholder="选择合适的工具"
-                            filterOption={filterOption}
-                            optionFilterProp="label"
-                            value={tool.value}
-                            onMouseDown={(e) => e.stopPropagation()}
-                            onChange={(e) => handleSkillChange(tool.id, e)}
-                            options={toolOptions}
-                        />
-                    </Form.Item>
-                    <Row gutter={16} style={{marginBottom: "6px", marginRight: 0, marginLeft: "-3%"}}>
-                        <Col span={22}>
-                            <span className="jade-font-size jade-font-color" style={{marginLeft: "6px"}}>工具流</span>
-                        </Col>
-                        <Col span={2} style={{paddingLeft: "3%"}}>
-                            <Button type="text" className="icon-button"
-                                    style={{height: "22px"}}
-                                    onClick={(event) => {
-                                        handleClick(event);
-                                    }}>
-                                <PlusOutlined/>
-                            </Button>
-                        </Col>
-                    </Row>
+            <Collapse bordered={false} className="jade-collapse-custom-background-color"
+                      defaultActiveKey={["skillPanel"]}>
+                {
+                    <Panel
+                            key={"skillPanel"}
+                            header={
+                                <div className="panel-header"
+                                     style={{display: 'flex', alignItems: 'center', justifyContent: "flex-start"}}>
+                                    <span className="jade-panel-header-font">技能</span>
+                                </div>
+                            }
+                            className="jade-panel"
+                    >
+                        <Row gutter={16} style={{marginBottom: "6px", marginRight: 0, marginLeft: "-3%"}}>
+                            <Col span={21}>
+                                <span className="jade-font-size jade-font-color" style={{marginLeft: "6px"}}>工具</span>
+                            </Col>
+                            {/*430演示工具不需要+号跳转，暂时屏蔽*/}
+                            {/*<Col span={3}>*/}
+                            {/*    <Button type="text" className="icon-button"*/}
+                            {/*            style={{height: "22px", marginLeft: "4px"}}*/}
+                            {/*            onClick={(event) => {*/}
+                            {/*                handleClick(event);*/}
+                            {/*            }}>*/}
+                            {/*        <PlusOutlined/>*/}
+                            {/*    </Button>*/}
+                            {/*</Col>*/}
+                        </Row>
+                        <Form.Item>
+                            <JadeStopPropagationSelect
+                                    mode="multiple"
+                                    showSearch
+                                    allowClear
+                                    className="jade-select"
+                                    placeholder="选择合适的工具"
+                                    filterOption={filterOption}
+                                    optionFilterProp="label"
+                                    value={tool.value}
+                                    onMouseDown={(e) => e.stopPropagation()}
+                                    onChange={(e) => handleSkillChange(tool.id, e)}
+                                    options={toolOptions}
+                            />
+                        </Form.Item>
+                        <Row gutter={16} style={{marginBottom: "6px", marginRight: 0, marginLeft: "-3%"}}>
+                            <Col span={22}>
+                                <span className="jade-font-size jade-font-color"
+                                      style={{marginLeft: "6px"}}>工具流</span>
+                            </Col>
+                            <Col span={2} style={{paddingLeft: "3%"}}>
+                                <Button type="text" className="icon-button"
+                                        style={{height: "22px"}}
+                                        onClick={(event) => {
+                                            handleAddWaterFlow(event);
+                                        }}>
+                                    <PlusOutlined/>
+                                </Button>
+                            </Col>
+                        </Row>
 
-                    <Form.Item>
-                        <JadeStopPropagationSelect
-                            mode="multiple"
-                            showSearch
-                            allowClear
-                            className="jade-select"
-                            placeholder="选择合适的工具流"
-                            filterOption={filterOption}
-                            optionFilterProp="label"
-                            value={workflow.value}
-                            onChange={(e) => handleSkillChange(workflow.id, e)}
-                            options={workflowOptions}
-                        />
-                    </Form.Item>
-                </Panel>
-            }
-        </Collapse>
+                        <Form.Item>
+                            <JadeStopPropagationSelect
+                                    mode="multiple"
+                                    showSearch
+                                    allowClear
+                                    className="jade-select"
+                                    placeholder="选择合适的工具流"
+                                    filterOption={filterOption}
+                                    optionFilterProp="label"
+                                    value={workflow.value}
+                                    onChange={(e) => handleSkillChange(workflow.id, e)}
+                                    options={workflowOptions}
+                            />
+                        </Form.Item>
+                    </Panel>
+                }
+            </Collapse>
     );
 }
