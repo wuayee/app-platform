399d19f1a5e827e43cbebd6aa66ebc08d1b56ab8
* <*>
2024-06-06 15:48:09 +0800

    Match-id-308d8de463e0a028e31c800d6d3bd79197e93134

--- carver/plugins/tool-repository-postgresql/src/main/resources/schema.sql
+++ carver/plugins/tool-repository-postgresql/src/main/resources/schema.sql
@@ -3,11 +3,11 @@ $$
 begin
 create table if not exists store_tool
 (
-    "id"             bigserial primary key                 not null,
+    "id"             bigserial   primary key               not null,
     "created_time"   timestamp   default current_timestamp not null,
     "updated_time"   timestamp   default current_timestamp not null,
-    "creator"        varchar(10)    default 'system'          not null,
-    "modifier"       varchar(10)    default 'system'          not null,
+    "creator"        varchar(10) default 'system'          not null,
+    "modifier"       varchar(10) default 'system'          not null,
     "name"           varchar(64)                           not null,
     "description"    text        default 'no desc'         not null,
     "schema"         json        default '{}'::json        not null,
@@ -16,19 +16,20 @@ create table if not exists store_tool
     "icon"           text,
     "unique_name"    char(36)                              not null,
     unique("unique_name")
-    );
+);
 comment on column store_tool.id is '工具的自增主键';
-        comment on column store_tool.created_time is '工具的创建时间';
-        comment on column store_tool.updated_time is '工具的更新时间';
-        comment on column store_tool.creator is '工具的创建者';
-        comment on column store_tool.modifier is '工具的修改者';
-        comment on column store_tool.name is '工具的名字';
-        comment on column store_tool.description is '工具的描述';
-        comment on column store_tool.schema is '工具的格式';
-        comment on column store_tool.runnables is '工具的运行描述';
-        comment on column store_tool.source is '工具的来源';
-        comment on column store_tool.icon is '工具的图标';
-        comment on column store_tool.unique_name is '工具的唯一标识';
+comment on column store_tool.created_time is '工具的创建时间';
+comment on column store_tool.updated_time is '工具的更新时间';
+comment on column store_tool.creator is '工具的创建者';
+comment on column store_tool.modifier is '工具的修改者';
+comment on column store_tool.name is '工具的名字';
+comment on column store_tool.description is '工具的描述';
+comment on column store_tool.schema is '工具的格式';
+comment on column store_tool.runnables is '工具的运行描述';
+comment on column store_tool.source is '工具的来源';
+comment on column store_tool.icon is '工具的图标';
+comment on column store_tool.unique_name is '工具的唯一标识';
+
 create table if not exists store_tag
 (
     "id"           bigserial primary key               not null,
@@ -39,13 +40,13 @@ create table if not exists store_tag
     "tool_unique_name"      char(36)                   not null,
     "name"         varchar(64)                         not null,
     unique ("tool_unique_name", "name")
-    );
+);
 comment on column store_tag.id is '标签的自增主键';
-        comment on column store_tag.created_time is '标签的创建时间';
-        comment on column store_tag.updated_time is '标签的更新时间';
-        comment on column store_tag.creator is '标签的创建者';
-        comment on column store_tag.modifier is '标签的修改者';
-        comment on column store_tag.name is '标签的名称';
-        comment on column store_tag.tool_unique_name is '工具的唯一标识';
+comment on column store_tag.created_time is '标签的创建时间';
+comment on column store_tag.updated_time is '标签的更新时间';
+comment on column store_tag.creator is '标签的创建者';
+comment on column store_tag.modifier is '标签的修改者';
+comment on column store_tag.name is '标签的名称';
+comment on column store_tag.tool_unique_name is '工具的唯一标识';
 end
 $$;
---
+++ carver/services/tool-service/src/main/java/com/huawei/jade/carver/tool/eco/TaskToolRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.jade.carver.tool.eco;
+
+/**
+ * 表示 {@link TaskTool} 的仓库。
+ *
+ * @author 季聿阶
+ * @since 2024-06-05
+ */
+public interface TaskToolRepository {}
