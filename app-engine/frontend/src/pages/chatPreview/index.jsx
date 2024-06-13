c0aa4e23393b0eaae7c4617932bd547493288696
* <*>
2024-06-13 14:11:24 +0800

    Match-id-0e7beb03a4b6cf74503a79bd37d8d67d6ddc1363

--- app-engine/frontend/src/pages/chatPreview/index.jsx
+++ app-engine/frontend/src/pages/chatPreview/index.jsx
@@ -180,8 +180,7 @@ const ChatPreview = (props) => {
     runningAppid.current = aipp_id;
     if (!wsCurrent.current) {
       const prefix = window.location.protocol === 'http:' ? 'ws' : 'wss';
-      // wsCurrent.current = new WebSocket(`${prefix}://${window.location.host}/api/jober/v1/api/aipp/wsStream?aippId=${aipp_id}&version=${version}`);
-      wsCurrent.current = new WebSocket(`${prefix}://80.11.128.86:30010/api/jober/v1/api/aipp/wsStream?aippId=${aipp_id}&version=${version}`);
+      wsCurrent.current = new WebSocket(`${prefix}://${window.location.host}/api/jober/v1/api/aipp/wsStream?aippId=${aipp_id}&version=${version}`);
       wsCurrent.current.onopen = () => {
         wsCurrent.current.send(JSON.stringify({'aippInstanceId': instanceId}));   
       }
