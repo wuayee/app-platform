951a76e3553bc26dc034ca9967349429393acf7a
* <*>
2024-06-14 16:25:22 +0800

    Match-id-f386cddc04056df276ca5573464c5c750a14631a

---
+++ eco/huggingface-pipeline/src/main/java/com/huawei/jade/fel/pipeline/huggingface/entity/HealthCheckRequest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.jade.fel.pipeline.huggingface.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 管道健康检查请求体（用于网关匹配路由）。
+ *
+ * @author 张庭怿
+ * @since 2024-06-13
+ */
+@AllArgsConstructor
+@Data
+public class HealthCheckRequest {
+    private String model;
+
+    private String task;
+}
