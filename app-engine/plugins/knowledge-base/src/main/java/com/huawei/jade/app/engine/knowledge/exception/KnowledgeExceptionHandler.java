0c23b42ee1f7e219e1ceb7d195e375faa0f47667
* <*>
2024-06-18 16:16:17 +0800

    Match-id-464d3eccf5b3b3a2c8ae1107bc1ac940fe216764

--- app-engine/plugins/knowledge-base/src/main/java/com/huawei/jade/app/engine/knowledge/exception/KnowledgeExceptionHandler.java
+++ app-engine/plugins/knowledge-base/src/main/java/com/huawei/jade/app/engine/knowledge/exception/KnowledgeExceptionHandler.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
 package com.huawei.jade.app.engine.knowledge.exception;
 
 import com.huawei.fit.http.annotation.ExceptionHandler;
@@ -5,52 +9,31 @@ import com.huawei.fit.http.annotation.ResponseStatus;
 import com.huawei.fit.http.protocol.HttpResponseStatus;
 import com.huawei.fitframework.annotation.Component;
 import com.huawei.fitframework.log.Logger;
-import com.huawei.fitframework.plugin.Plugin;
-import com.huawei.fitframework.util.StringUtils;
-
+import com.huawei.jade.app.engine.knowledge.common.KnowledgeRsp;
 import com.huawei.jade.app.engine.knowledge.service.exception.ServiceException;
+
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-
-
+/**
+ * 处理知识库Controller相关异常。
+ *
+ * @since 2024-06-18
+ */
 @Component
 @RequiredArgsConstructor
 public class KnowledgeExceptionHandler {
     private static final Logger log = Logger.get(KnowledgeExceptionHandler.class);
 
-    private final Plugin plugin;
-
-//    /**
-//     * handleThrowable
-//     *
-//     * @param exception exception
-//     * @return Rsp<String>
-//     */
-//    @ExceptionHandler(Throwable.class)
-//    @ResponseStatus(HttpResponseStatus.OK)
-//    public Rsp<String> handleThrowable(Throwable exception) {
-//        log.error(exception.getClass().getName(), exception);
-//        return Rsp.err(AippErrCode.UNKNOWN.getErrorCode(), AippErrCode.UNKNOWN.getMessage());
-//    }
-//
-//    /**
-//     * ServiceException 处理器。
-//     *
-//     * @param exception 异常的 {@link ServiceException}。
-//     * @return 包含异常信息的通用返回体的 {@link Rsp}{@code <}{@link String}{@code >}
-//     */
-//    @ExceptionHandler(ServiceException.class)
-//    @ResponseStatus(HttpResponseStatus.OK)
-//    public Rsp<String> handleAippException(ServiceException exception) {
-//        log.error(exception.getClass().getName(), exception);
-//        return Rsp.err(400,
-//                getLocaleMessage(String.valueOf(exception.getCode()),
-//                        exception.getMessage(),
-//                        exception.getArgs(),
-//                        exception.getContext()));
-//    }
-
+    /**
+     * ServiceException 处理器。
+     *
+     * @param exception 异常的 {@link ServiceException}。
+     * @return 包含异常信息的通用返回体的 {@link KnowledgeRsp}{@code <}{@link String}{@code >}
+     */
+    @ExceptionHandler(ServiceException.class)
+    @ResponseStatus(HttpResponseStatus.BAD_REQUEST)
+    public KnowledgeRsp handleKnowledgeException(ServiceException exception) {
+        log.error(exception.getClass().getName(), exception);
+        return KnowledgeRsp.err(400, exception.getMessage());
+    }
 }
