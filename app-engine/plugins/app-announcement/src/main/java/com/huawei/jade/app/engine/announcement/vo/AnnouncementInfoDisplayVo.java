f6e90141607253943cbc5dbe26ee6fb94ea3a97c
* <*>
2024-06-18 17:44:35 +0800

    Match-id-d87127a77fef097754cd9760207f979abc03e65b

---
+++ app-engine/plugins/app-announcement/src/main/java/com/huawei/jade/app/engine/announcement/vo/AnnouncementInfoDisplayVo.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.jade.app.engine.announcement.vo;
+
+import com.huawei.fitframework.annotation.Property;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * AnnouncementInfoDisplayVo类消息处理策略.
+ *
+ * @author z00591349
+ * @since 2024-06-18
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnnouncementInfoDisplayVo {
+    @Property(description = "公告内容")
+    private String content;
+
+    @Property(description = "详情地址")
+    private String detailsUrl;
+
+    @Property(description = "公告信息类型")
+    private String announcementType;
+}
