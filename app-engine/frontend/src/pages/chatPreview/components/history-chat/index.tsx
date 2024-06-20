4a3c86cb9ea51ee54ed85bb4ca07d215cb85c259
* <*>
2024-06-20 13:58:59 +0800

    Match-id-00fa1257e32eb76fd8d53dbdaa4e323259f29568

--- app-engine/frontend/src/pages/chatPreview/components/history-chat/index.tsx
+++ app-engine/frontend/src/pages/chatPreview/components/history-chat/index.tsx
@@ -65,8 +65,6 @@ const HistoryChatDrawer: React.FC<HistoryChatProps> = ({ openHistorySignal }) =>
       limit: 100
     };
     setRequestInfo(requestBody);
-    const chatRes = await getChatList(tenantId, requestBody);
-    setData(chatRes?.data);
   }
 
   const items: MenuProps["items"] = [
@@ -133,7 +131,7 @@ const HistoryChatDrawer: React.FC<HistoryChatProps> = ({ openHistorySignal }) =>
     if (appInfo?.id) {
       getAippId();
     }
-  }, [appInfo])
+  }, [appInfo.id])
 
   const getLastContext = async () => {
     const chatListRes = await getChatDetail(tenantId, chatId, requestInfo);
@@ -183,7 +181,7 @@ const HistoryChatDrawer: React.FC<HistoryChatProps> = ({ openHistorySignal }) =>
         <Input placeholder="搜索..." prefix={<SearchOutlined />} disabled />
       </div>
       <div className="history-wrapper">
-        {data?.map((item) => (
+        {data?.slice(0, 30).map((item) => (
           <div className="history-item" key={item?.chat_id} onClick={() => { currentChat.current = item; }}>
             <div className="history-item-content">
               <div className="history-item-header">
