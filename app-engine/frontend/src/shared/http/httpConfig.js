3eb237e7d7425b177aa1fb3dae0fe05d219e562a
* <*>
2024-06-26 16:10:26 +0800

    Match-id-7002d7e257641077d96e393c378312a667346a88

--- app-engine/frontend/src/shared/http/httpConfig.js
+++ app-engine/frontend/src/shared/http/httpConfig.js
@@ -11,7 +11,7 @@ export const httpUrlMap = {
     WS_AUDIO_URL: 'wss://tzaip-beta.paas.huawei.com/tzaip/api/hisp/api/v1/platform/app/asr',
     MODEL_URL: '/modelApi',
     APP_URL: '/app',
-    MODEL_LIST_URL: '/v1',
+    MODEL_LIST_URL: '/api/model_io_manager/v1',
     PLUGIN_URL: '/api/jober',
     MODEL_BASE_URL: 'modelbase',
     MODEL_TRAINING_URL: 'modeltrain',
