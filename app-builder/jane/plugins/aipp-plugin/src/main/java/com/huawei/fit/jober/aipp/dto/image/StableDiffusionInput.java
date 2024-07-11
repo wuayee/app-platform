5d6c3efa4f319f48942902423dc4487b9beb2813
* <*>
2024-07-11 10:00:48 +0800

    Match-id-9e9f3b77e23701ed28cb583a4f209177b1d5c8c8

---
+++ app-builder/jane/plugins/aipp-plugin/src/main/java/com/huawei/fit/jober/aipp/dto/image/StableDiffusionInput.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.fit.jober.aipp.dto.image;
+
+import lombok.Data;
+
+/**
+ * StableDiffusion pipeline模型的输入
+ *
+ * @author 易文渊
+ * @since 2024-06-28
+ */
+@Data
+public class StableDiffusionInput {
+    private String prompt;
+    private String negativePrompt;
+}
---
+++ app-builder/jane/plugins/aipp-plugin/src/main/java/com/huawei/fit/jober/aipp/tool/StableDiffusionParserTool.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+package com.huawei.fit.jober.aipp.tool;
+
+import com.huawei.fit.jober.aipp.dto.image.StableDiffusionInput;
+import com.huawei.fitframework.annotation.Genericable;
+
+/**
+ * StableDiffusion模型json入参解析
+ *
+ * @author 易文渊
+ * @since 2024-06-28
+ */
+public interface StableDiffusionParserTool {
+    /**
+     * 解析json字符串。
+     *
+     * @param input StableDiffusion模型json入参 {@link String}。
+     * @return 解析后的SD多模态模型输入 {@link StableDiffusionInput}{@code >}。
+     */
+    @Genericable(id = "com.huawei.fit.jober.aipp.tool.sd.parse")
+    StableDiffusionInput parse(String input);
+}
