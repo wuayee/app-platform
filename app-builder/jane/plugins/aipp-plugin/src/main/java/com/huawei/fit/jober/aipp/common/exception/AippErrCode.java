a9cb2dc6d95f943333d6069b03fc780b07a62b5d
* <*>
2024-07-15 15:06:29 +0800

    Match-id-94515bda31eadb5dccd64d747440f5fb100528d1

--- app-builder/jane/plugins/aipp-plugin/src/main/java/com/huawei/fit/jober/aipp/common/exception/AippErrCode.java
+++ app-builder/jane/plugins/aipp-plugin/src/main/java/com/huawei/fit/jober/aipp/common/exception/AippErrCode.java
@@ -194,7 +194,7 @@ public enum AippErrCode implements ErrorCode {
     /**
      * 创建调试aipp失败
      */
-    CREATE_DEBUG_AIPP_FAILED(90002907, "调试失败，请检查流程配置是否正确: {0}。"),
+    CREATE_DEBUG_AIPP_FAILED(90002907, "调试失败，请检查流程配置是否正确: {0}"),
 
     /**
      * 不支持的数据格式
