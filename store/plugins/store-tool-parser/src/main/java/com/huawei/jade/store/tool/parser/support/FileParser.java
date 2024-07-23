ee0dbfa8c2b31ff0b5084b74d046ada21dd0ee8c
* <*>
2024-07-23 14:27:03 +0800

    Match-id-4ffeafcbae18e273b0bdaf0d1f1a6623fbfe7f41

--- store/plugins/store-tool-parser/src/main/java/com/huawei/jade/store/tool/parser/support/FileParser.java
+++ store/plugins/store-tool-parser/src/main/java/com/huawei/jade/store/tool/parser/support/FileParser.java
@@ -4,17 +4,16 @@
 
 package com.huawei.jade.store.tool.parser.support;
 
+import com.huawei.fitframework.util.ObjectUtils;
 import com.huawei.jade.store.entity.transfer.PluginData;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 /**
  * 根据文件路径解析文件。
@@ -23,50 +22,43 @@ import java.util.stream.StreamSupport;
  * @since 2024-06-18
  */
 public class FileParser {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String RUNNABLES = "runnables";
     private static final String SCHEMA = "schema";
     private static final String NAME = "name";
     private static final String DESCRIPTION = "description";
+    private static final String TAGS = "tags";
+    private static final String COMMA = ",";
 
     /**
      * 表示基于工具名列表匹配工具数据。
      *
-     * @param toolNode 给定的工具元数据的 {@link JsonNode}。
+     * @param toolFile 给定的工具元数据的 {@link File}。
      * @param toolNames 给定的工具名列表的 {@link Set}{@code <}{@link String}{@code >}。
      * @return 匹配成功的工具数据。
-     * @throws IOException 表示解析数据失败时抛出异常。
      */
-    public static PluginData getPluginData(JsonNode toolNode, Set<String> toolNames) throws IOException {
-        JsonNode schemaNode = toolNode.path(SCHEMA);
-        String methodName = schemaNode.path(NAME).asText();
-        if (!toolNames.contains(methodName)) {
-            return new PluginData();
-        }
-        String methodDescription = schemaNode.path(DESCRIPTION).asText();
+    public static PluginData getPluginData(Map<String, Object> toolFile, String toolNames) {
+        Set<String> toolNamesSet = Arrays.stream(toolNames.trim().split(COMMA)).collect(Collectors.toSet());
+        Map<String, Object> schemaNode = ObjectUtils.cast(toolFile.get(SCHEMA));
+        Object name = schemaNode.get(NAME);
         PluginData pluginData = new PluginData();
-        pluginData.setName(methodName);
-        pluginData.setDescription(methodDescription);
-        pluginData.setRunnables(getToolInfo(toolNode, RUNNABLES));
-        pluginData.setSchema(getToolInfo(toolNode, SCHEMA));
+        pluginData.setSchema(schemaNode);
+        if (name instanceof String) {
+            String methodName = (String) name;
+            if (!toolNamesSet.contains(methodName)) {
+                return new PluginData();
+            }
+            pluginData.setName(methodName);
+        }
 
-        JsonNode tagsInfo = toolNode.path("tags");
-        Set<String> tags =
-                StreamSupport.stream(tagsInfo.spliterator(), false).map(JsonNode::asText).collect(Collectors.toSet());
-        pluginData.setTags(tags);
+        Object methodDescription = schemaNode.get(DESCRIPTION);
+        if (methodDescription instanceof String) {
+            pluginData.setDescription((String) methodDescription);
+        }
+
+        Map<String, Object> runnables = ObjectUtils.cast(toolFile.get(RUNNABLES));
+        pluginData.setRunnables(runnables);
+        List<String> tags = ObjectUtils.cast(toolFile.get(TAGS));
+        pluginData.setTags(new HashSet<>(tags));
         return pluginData;
-    }
-
-    /**
-     * 表示将 json 数据的对应字段的信息转换为 map 类型的数据。
-     *
-     * @param toolInfo 给定的工具元数据的 {@link JsonNode}。
-     * @param fieldName 给定的字段名的 {@link String}。
-     * @return 字段名下的数据的 {@link Map}{@code <}{@link String}{@code , }{@link Object}{@code >}。
-     * @throws IOException 表示解析数据失败时抛出异常。
-     */
-    public static Map<String, Object> getToolInfo(JsonNode toolInfo, String fieldName) throws IOException {
-        return OBJECT_MAPPER.readValue(toolInfo.path(fieldName).traverse(),
-                new TypeReference<Map<String, Object>>() {});
     }
 }
--- store/plugins/store-tool-parser/src/test/java/com/huawei/jade/store/tool/parser/support/FileParserTest.java
+++ store/plugins/store-tool-parser/src/test/java/com/huawei/jade/store/tool/parser/support/FileParserTest.java
@@ -6,22 +6,21 @@ package com.huawei.jade.store.tool.parser.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.huawei.fit.serialization.json.jackson.JacksonObjectSerializer;
+import com.huawei.fitframework.util.ObjectUtils;
 import com.huawei.jade.store.entity.transfer.PluginData;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 /**
  * 表示 {@link FileParser} 的单元测试。
@@ -31,27 +30,56 @@ import java.util.Set;
  */
 @DisplayName("测试 FileParser 类")
 public class FileParserTest {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private List<Object> toolList = null;
+
+    @BeforeEach
+    void setup() throws IOException {
+        String sourceFolderPath = "src/test/resources/tools.json";
+        JacksonObjectSerializer jacksonObjectSerializer = new JacksonObjectSerializer(null, null, null);
+        try (InputStream in = Files.newInputStream(Paths.get(sourceFolderPath))) {
+            Map<String, Object> jsonData = jacksonObjectSerializer.deserialize(in, Map.class);
+            this.toolList = ObjectUtils.cast(jsonData.get("tools"));
+        }
+    }
 
     @Test
     @DisplayName("解析 json 文件，匹配数据成功")
-    void givenValidJsonFileThenGetPluginDataSuccessfully() throws IOException {
-        Set<String> toolNames = new HashSet<>();
-        toolNames.add("add itself");
-        String sourceFolderPath = "src/test/resources/tools.json";
-        Path filePath = Paths.get(sourceFolderPath);
-        String jsonContent = new String(Files.readAllBytes(filePath));
-        JsonNode jsonNode = OBJECT_MAPPER.readTree(jsonContent);
-        JsonNode toolsNode = jsonNode.get("tools");
-        List<PluginData> pluginsData = new ArrayList<>();
-        for (JsonNode node : toolsNode) {
-            PluginData pluginData = FileParser.getPluginData(node, toolNames);
+    void givenValidJsonFileThenGetPluginDataSuccessfully() {
+        assert this.toolList != null;
+        assertThat(this.toolList.size()).isEqualTo(3);
+        Map<String, Object> tool = ObjectUtils.cast(this.toolList.get(0));
+        Map<String, Object> schema = ObjectUtils.cast(tool.get("schema"));
+        assertThat(schema.get("name")).isEqualTo("add list");
+        assertThat(schema.get("description")).isEqualTo("This method adds two list");
+
+        tool = ObjectUtils.cast(this.toolList.get(1));
+        schema = ObjectUtils.cast(tool.get("schema"));
+        assertThat(schema.get("name")).isEqualTo("add itself");
+        assertThat(schema.get("description")).isEqualTo("This method adds two integers");
+
+        tool = ObjectUtils.cast(this.toolList.get(2));
+        schema = ObjectUtils.cast(tool.get("schema"));
+        assertThat(schema.get("name")).isEqualTo("name for tool");
+        assertThat(schema.get("description")).isEqualTo("description for tool.");
+    }
+
+    @Test
+    @DisplayName("给定有效工具名，数据匹配成功")
+    void givenValidToolNamesThenGetToolDataSuccessfully() {
+        String validToolNames = "add list,add itself";
+        List<PluginData> list = new ArrayList<>();
+        for (Object toolInfo : this.toolList) {
+            Map<String, Object> toolMap = ObjectUtils.cast(toolInfo);
+            PluginData pluginData = FileParser.getPluginData(toolMap, validToolNames);
             if (pluginData.getName() != null) {
-                pluginsData.add(pluginData);
+                list.add(pluginData);
             }
         }
-        assertThat(pluginsData.size()).isEqualTo(1);
-        PluginData pluginData = pluginsData.get(0);
+        assertThat(list.size()).isEqualTo(2);
+        PluginData pluginData = list.get(0);
+        assertThat(pluginData.getName()).isEqualTo("add list");
+        assertThat(pluginData.getDescription()).isEqualTo("This method adds two list");
+        pluginData = list.get(1);
         assertThat(pluginData.getName()).isEqualTo("add itself");
         assertThat(pluginData.getDescription()).isEqualTo("This method adds two integers");
     }
