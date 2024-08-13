48fec99006ecbd3d5664f9a4e5551ada9348071e
* <*>
2024-08-13 17:43:57 +0800

    Match-id-218ef310078170aca5770e5d09a07739a05aad43

---
+++ eco/algorithm/python/plugins/legend_cleaner_plugin/src/fitable.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+"""
+since: 2023/11/02 22:00
+"""
+
+from typing import List
+
+from common.model import FlowData
+from .plugin import LegendCleanerPlugin
+
+
+def clean_legend(flow_data_list: List[FlowData]):
+    contents = [flow_data.passData for flow_data in flow_data_list]
+    contents = LegendCleanerPlugin().execute(contents)
+    return [FlowData(businessData=flow_data.businessData, passData=content)
+            for flow_data, content in zip(flow_data_list, contents)]
