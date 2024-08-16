84ae9b39c630af168d9d46b36c63d4f7621b8418
* <*>
2024-08-16 11:01:10 +0800

    Match-id-803fc48f2a42ea26e5b77f6b84b2d41b0813e747

--- app-engine/plugins/eval-task/src/test/java/com/huawei/jade/app/engine/task/service/impl/EvalTaskServiceImplTest.java
+++ app-engine/plugins/eval-task/src/test/java/com/huawei/jade/app/engine/task/service/impl/EvalTaskServiceImplTest.java
@@ -4,22 +4,29 @@
 
 package com.huawei.jade.app.engine.task.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.huawei.fitframework.annotation.Fit;
 import com.huawei.fitframework.test.annotation.FitTestWithJunit;
 import com.huawei.fitframework.test.annotation.Mock;
+import com.huawei.jade.app.engine.task.dto.EvalTaskQueryParam;
 import com.huawei.jade.app.engine.task.entity.EvalTaskEntity;
 import com.huawei.jade.app.engine.task.mapper.EvalTaskMapper;
 import com.huawei.jade.app.engine.task.service.EvalTaskService;
+import com.huawei.jade.common.vo.PageVo;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 表示 {@link EvalTaskServiceImpl} 的测试集。
@@ -53,5 +60,41 @@ public class EvalTaskServiceImplTest {
 
         this.evalTaskService.createEvalTask(entity);
         verify(this.evalTaskMapper, times(1)).create((any()));
+    }
+
+    @Test
+    @DisplayName("查询全量评估任务元数据成功")
+    void shouldOkWhenListEvalTask() {
+        Long taskId = 1L;
+        String name = "task1";
+        String description = "eval task";
+        String status = "published";
+        String appId = "123456";
+        String workflowId = "flow1";
+
+        EvalTaskEntity entity = new EvalTaskEntity();
+        entity.setId(taskId);
+        entity.setName(name);
+        entity.setDescription(description);
+        entity.setStatus(status);
+        entity.setAppId(appId);
+        entity.setWorkflowId(workflowId);
+        List<EvalTaskEntity> entities = Collections.singletonList(entity);
+
+        when(this.evalTaskMapper.listEvalTask(any())).thenReturn(entities);
+        when(this.evalTaskMapper.countEvalTask(any())).thenReturn(1);
+
+        EvalTaskQueryParam queryParam = new EvalTaskQueryParam();
+        PageVo<EvalTaskEntity> response = this.evalTaskService.listEvalTask(queryParam);
+
+        EvalTaskEntity firstEntity = response.getItems().get(0);
+        assertThat(response).extracting(PageVo::getTotal, r -> r.getItems().size()).containsExactly(1, 1);
+        assertThat(firstEntity).extracting(EvalTaskEntity::getId,
+                        EvalTaskEntity::getName,
+                        EvalTaskEntity::getDescription,
+                        EvalTaskEntity::getStatus,
+                        EvalTaskEntity::getAppId,
+                        EvalTaskEntity::getWorkflowId)
+                .containsExactly(taskId, name, description, status, appId, workflowId);
     }
 }
--- app-engine/plugins/eval-task/src/test/resources/sql/test_create_table.sql
+++ app-engine/plugins/eval-task/src/test/resources/sql/test_create_table.sql
@@ -1,3 +1,5 @@
+drop table if exists t_app_engine_eval_task;
+
 create table if not exists t_app_engine_eval_task
 (
     "id"               bigserial primary key                 not null,
@@ -9,5 +11,5 @@ create table if not exists t_app_engine_eval_task
     "created_by"       varchar(64) default 'system'          not null,
     "updated_by"       varchar(64) default 'system'          not null,
     "app_id"           varchar(255)                          not null,
-    "workflow_id"      char(32)                              not null
+    "workflow_id"      varchar(32)                           not null
 );
