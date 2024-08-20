2d9a26c0f72c7040a107a12154eca31d8131c23f
* <*>
2024-08-20 16:48:29 +0800

    Match-id-bbb8a27f476569d429e2f80eb828efda0e3df52d

--- multi-env-support/plugins/authentication-default/src/test/java/com/huawei/jade/authentication/AuthenticationServiceImplTest.java
+++ multi-env-support/plugins/authentication-default/src/test/java/com/huawei/jade/authentication/AuthenticationServiceImplTest.java
@@ -23,35 +23,34 @@ import org.mockito.Mockito;
 
 })
 public class AuthenticationServiceImplTest {
-    private static final String TOKEN_KEY = "X-Uni-Crsf-Token";
+    private static final String USERNAME_KEY = "username";
 
-    private static final String TOKEN_ID_KEY = "X-Uni-Crsf-Token-Id";
+    private static final String DEFAULT_USERNAME = "Jade";
 
+    private static final String MODEL_ENGINE_USERNAME = "admin";
 
     @Test
-    void testGetUserName_CookiesNotExist() {
+    void testGetUserName_CookiesNotContain() {
         HttpClassicServerRequest request = Mockito.mock(HttpClassicServerRequest.class);
         Mockito.when(request.cookies()).thenReturn(new DefaultCookieCollection());
 
         AuthenticationServiceImpl authenticationService = new AuthenticationServiceImpl();
 
         String userName = authenticationService.getUserName(request);
-        assertEquals("Jade", userName);
+        assertEquals(DEFAULT_USERNAME, userName);
     }
 
     @Test
-    void testGetUserName_CookiesExist() {
+    void testGetUserName_CookiesContain() {
         HttpClassicServerRequest request = Mockito.mock(HttpClassicServerRequest.class);
-        Cookie tokenCookie = Cookie.builder().name(TOKEN_KEY).value("tokenValue").build();
-        Cookie tokenIdCookie = Cookie.builder().name(TOKEN_ID_KEY).value("tokenIdValue").build();
+        Cookie userNameCookie = Cookie.builder().name(USERNAME_KEY).value(MODEL_ENGINE_USERNAME).build();
         DefaultCookieCollection cookies = new DefaultCookieCollection();
-        cookies.add(tokenCookie);
-        cookies.add(tokenIdCookie);
+        cookies.add(userNameCookie);
         Mockito.when(request.cookies()).thenReturn(cookies);
 
         AuthenticationServiceImpl authenticationService = new AuthenticationServiceImpl();
 
         String userName = authenticationService.getUserName(request);
-        assertEquals("admin", userName);
+        assertEquals(MODEL_ENGINE_USERNAME, userName);
     }
 }
