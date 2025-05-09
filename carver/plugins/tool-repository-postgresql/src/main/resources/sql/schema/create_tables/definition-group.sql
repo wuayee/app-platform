3e86715df016fee204344b57e3e23bd5dc730227
wuayee <532203926@qq.com>
2025-05-09 11:11:54 +0800

    [appBuilder] 归档sql内置语句，并加入执行脚本 (#175)
    Co-authored-by: wuayee <wuayee@noreply.gitcode.com>

--- carver/plugins/tool-repository-postgresql/src/main/resources/sql/schema/create_tables/definition-group.sql
+++ carver/plugins/tool-repository-postgresql/src/main/resources/sql/schema/create_tables/definition-group.sql
@@ -1,5 +1,6 @@
-drop table if exists store_definition_group;
-
+do
+$$
+begin
 create table if not exists store_definition_group
 (
     "id"             bigserial   primary key               not null,
@@ -10,7 +11,7 @@ create table if not exists store_definition_group
     "name"           varchar(256)                          not null,
     "summary"        text        default 'no summary'      not null,
     "description"    text        default 'no desc'         not null,
-    "extensions"     text        default 'no extensions'   not null,
+    "extensions"     json        default '{}'::json        not null,
     unique("name")
     );
 comment on column store_definition_group.id is '定义组的自增主键';
@@ -22,3 +23,5 @@ comment on column store_definition_group.name is '定义组的名字';
 comment on column store_definition_group.summary is '定义组的摘要';
 comment on column store_definition_group.description is '定义组的描述';
 comment on column store_definition_group.extensions is '定义组的扩展';
+end
+$$;
