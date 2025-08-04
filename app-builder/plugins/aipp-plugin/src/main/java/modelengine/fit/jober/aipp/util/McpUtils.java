1517dcfe56df3d78953b784058659ee1b308dccd
song <271667068@qq.com>
2025-08-04 21:38:43 +0800

    [app-builder] mcp server compatible (#366)

--- app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/McpUtils.java
+++ app-builder/plugins/aipp-plugin/src/main/java/modelengine/fit/jober/aipp/util/McpUtils.java
@@ -6,7 +6,10 @@
 
 package modelengine.fit.jober.aipp.util;
 
-import modelengine.fitframework.inspection.Validation;
+import modelengine.fitframework.util.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * 大模型上下文协议相关工具方法。
@@ -15,19 +18,24 @@ import modelengine.fitframework.inspection.Validation;
  * @since 2025-07-11
  */
 public class McpUtils {
-    private static final String SSE_ENDPOINT_SPLIT_DELIMITER = "/";
-
     /**
      * 获取 {@code baseUrl} 部分。
      *
      * @param url 目标地址。
      * @return {@code baseUrl} 部分。
-     * @throws IllegalArgumentException 当目标地址不包含 {@code sseEndpoint} 时。
+     * @throws IllegalArgumentException 当目标地址不合法时。
      */
     public static String getBaseUrl(String url) {
-        String[] splits = url.split(SSE_ENDPOINT_SPLIT_DELIMITER);
-        Validation.greaterThan(splits.length, 3, "The url is wrong. [url={0}]", url);
-        return url.substring(0, url.length() - splits[splits.length - 1].length() - 1);
+        try {
+            URI uri = new URI(url);
+            String baseUrl = uri.getScheme() + "://" + uri.getHost();
+            if (uri.getPort() != -1) {
+                baseUrl += ":" + uri.getPort();
+            }
+            return baseUrl;
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(StringUtils.format("The url is wrong. [url={0}]", url));
+        }
     }
 
     /**
@@ -35,11 +43,14 @@ public class McpUtils {
      *
      * @param url 目标地址。
      * @return {@code sseEndpoint} 部分。
-     * @throws IllegalArgumentException 当目标地址不包含 {@code sseEndpoint} 时。
+     * @throws IllegalArgumentException 当目标地址不合法时。
      */
     public static String getSseEndpoint(String url) {
-        String[] splits = url.split(SSE_ENDPOINT_SPLIT_DELIMITER);
-        Validation.greaterThan(splits.length, 3, "The url is wrong. [url={0}]", url);
-        return SSE_ENDPOINT_SPLIT_DELIMITER + splits[splits.length - 1];
+        try {
+            URI uri = new URI(url);
+            return uri.getPath();
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(StringUtils.format("The url is wrong. [url={0}]", url));
+        }
     }
 }
